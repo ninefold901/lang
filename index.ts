acf48f3e070b211f6889a27bd2bbf0c391ef27e1
11 <11@11>
2023-06-14 23:59:22 +0800

    feat: add question

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
-function main() {
+import * as dotenv from 'dotenv';
+import { OpenAI } from 'langchain/llms/openai';
+
+dotenv.config();
+
+const model = new OpenAI({
+  modelName: 'gpt-3.5-turbo',
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  timeout: 30000, // ms?
+});
+
+async function main() {
   console.log('hi');
+  const res = await model.call('who are you?');
+  console.log(res);
 }
 
 main();
